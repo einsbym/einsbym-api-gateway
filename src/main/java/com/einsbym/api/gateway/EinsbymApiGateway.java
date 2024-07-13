eb17eba067651606df337039d303eb864a189e74
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-07-12 23:00:34 -0300

    feat: add jaeger in gateway add jaeger in gateway feat: add jaeger in gateway

--- src/main/java/com/einsbym/api/gateway/EinsbymApiGateway.java
+++ src/main/java/com/einsbym/api/gateway/EinsbymApiGateway.java
@@ -1,13 +1,23 @@
 package com.einsbym.api.gateway;
 
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class EinsbymApiGateway {
     public static void main(String[] args) {
         SpringApplication.run(EinsbymApiGateway.class, args);
+    }
+
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+        return OtlpHttpSpanExporter.builder()
+                .setEndpoint(url)
+                .build();
     }
 }
